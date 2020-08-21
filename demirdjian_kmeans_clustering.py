fb4b24bc3fa229cf14994e097f397c53e73cfc31
Jerry Xiong <33706493+SmellyPickle@users.noreply.github.com>
2020-08-21 14:26:34 -0400

    Update and rename kmeans_clustering.py to demirdjian_kmeans_clustering.py

---
+++ demirdjian_kmeans_clustering.py
@@ -0,0 +1,44 @@
+import numpy as np
+from scipy.stats import zscore
+from sklearn.cluster import MiniBatchKMeans
+
+num_clusters = (60, 60, 60)
+
+lon = np.linspace(-179.95, 179.95, 3600)
+lat = np.linspace(-59.95, 59.95, 1200)
+average_prec = np.transpose(np.loadtxt('data/average_prec.txt'))[300:1500]
+extreme_prec = np.loadtxt('data/extreme_prec_95.txt')[300:1500]
+topography = np.transpose(np.loadtxt('data/Topography.txt'))[300:1500]
+
+lon_data, lat_data = np.meshgrid(lon, lat)
+assert lon_data.shape == lat_data.shape == average_prec.shape == extreme_prec.shape == topography.shape == (1200, 3600)
+clust_data = np.concatenate([
+    zscore(lon_data.reshape(-1, 1)),
+    zscore(lat_data.reshape(-1, 1)),
+    zscore(average_prec.reshape(-1, 1)),
+    zscore(extreme_prec.reshape(-1, 1)),
+    zscore(topography.reshape(-1, 1))
+], axis=1)
+
+labels = np.zeros(clust_data.shape[0], dtype=int)
+for k in num_clusters:
+    print(f'Starting clustering stage with {k} centers')
+    new_labels = np.zeros_like(labels, dtype=int)
+    for label in np.unique(labels):
+        mask = (labels == label)
+        sz = np.count_nonzero(mask)
+        clust_subset_data = clust_data[mask]
+        if sz == 0:
+            continue
+        elif sz < k:
+            subset_labels = np.zeros(sz)
+        else:
+            subset_labels = MiniBatchKMeans(n_clusters=k).fit_predict(clust_subset_data)
+        if np.count_nonzero(labels != label) > 0:
+            subset_labels += np.max(new_labels) + 1
+        new_labels[labels == label] = subset_labels
+    labels = new_labels
+labels = labels.reshape(1200, 3600)
+np.savetxt(f'data/kmeans_{"".join(str(x) for x in num_clusters)}_clusters.txt', labels)
+
+print(f'Created {len(np.unique(labels))} clusters, average cluster size is {clust_data.shape[0] / len(np.unique(labels))}')
