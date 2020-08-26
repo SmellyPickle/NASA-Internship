1fd8d500acf6046ef2f011e6004d4150b56a9f93
Jerry Xiong <33706493+SmellyPickle@users.noreply.github.com>
2020-08-25 23:04:14 -0400

    Update view_cluster_size_distribution.py

--- view_cluster_size_distribution.py
+++ view_cluster_size_distribution.py
@@ -1,17 +1,20 @@
+"""Plots cluster size distributions"""
+
 import math
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-labels = np.loadtxt('data/modified_hdbscan_clusters.txt')
+labels = np.loadtxt('data/ga_insp_clusters.txt')
 counts = pd.Series(labels.flatten()).value_counts()
 fig, axes = plt.subplots(ncols=2, constrained_layout=True)
 
-print(np.min(counts))
-
 axes[0].set_title('Cluster size distribution')
-bins = min((x for x in range(min(20, np.ptp(counts) + 1), 60) if (np.ptp(counts) + 1) % x == 0),
-           key=lambda x: abs(x - 30))
+try:
+    bins = min((x for x in range(min(20, np.ptp(counts) + 1), 60) if (np.ptp(counts) + 1) % x == 0),
+               key=lambda x: abs(x - 30))
+except ValueError:
+    bins = 30
 axes[0].hist(counts, bins=bins)
 
 axes[1].set_title('Cluster size distribution (small clusters)')
